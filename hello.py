d39ea151c5b9bd5181d61f5f05517321a30b41d9
ykwoshia <ykwoshia@126.com>
2016-02-19 16:58:49 +0800

    first commit

---
+++ hello.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python
+from random import randint
+
+
+
+
+#  ## part1
+#  name = "Crossin"
+#  myVar = 123
+#  price = 5.99
+#  visible = True
+
+#  print name
+#  print myVar
+#  print price
+#  print visible
+
+#  pprint 2 < 5
+
+#  ##  part 2
+
+#  num = randint(1,100)
+#  print "guess"
+#  answer = input()
+#  while answer != num :
+    #  if answer < num :
+        #  print '%d is too small' % answer
+        #  answer = input()
+    #  if answer > num :
+        #  print '%d is too big' %answer
+        #  answer = input()
+#  print 'equal'
+
+## part3 sum
+#  a = 1
+ #  sum = 0
+  #  while a <= 100 :
+ #  sum += a
+    #  a += 1 
+
+#  print sum
+#  sum = 0
+#  for a in range(1, 101):
+    #  sum += a
+
+#  print sum
+
+
+print bool(-123)
+print bool(0)
+print bool('abc')
+print bool('False')
+print bool('')
+
