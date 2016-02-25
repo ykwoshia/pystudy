c2343fd52f3d334df6114722b6127774d0bdc124
ykwoshia <ykwoshia@126.com>
2016-02-25 17:28:09 +0800

    p60.txt line 1600

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from random import randint
+from random import choice
 
 
 
@@ -46,9 +47,60 @@ from random import randint
 #  print sum
 
 
-print bool(-123)
-print bool(0)
-print bool('abc')
-print bool('False')
-print bool('')
+## part 4 function
+#  #  def sayhello(a):
+    #  #  print "hello %s" % a
+#  #  def plus(a,b):
+    #  #  print a+b
+#  #  sayhello("Moto");
+#  #  plus(3,2)
+
+#  def isEqual(num1, num2):
+    #  if num1<num2:
+        #  print 'too small'
+        #  return False
+    #  elif num1>num2:
+        #  print 'too big'
+        #  return False
+    #  else:
+        #  print 'bingo'
+        #  return True
+
+#  num = randint(1, 10000)
+#  print 'Guess what I think?'
+#  bingo = False
+#  while bingo == False:
+   #  answer = input()
+   #  bingo = isEqual(answer, num)
+
+
+### part5 list 
+
+#  l = [1,2,3,4,5,6,7]
+#  l.append(8)
+#  print l
+#  del l[0]
+#  print l
+
+#  l = [365, 'everyday', 0.618, True]
+#  m = l[1:3]
+#  print m
+
+#  print 'chose one side to shoot:'
+#  print 'left, center, right'
+#  you = raw_input()
+#  print 'You kicked ' + you
+#  direction = ['left', 'center', 'right']
+#  com = choice(direction)
+#  print 'Computer saved ' + com
+#  if you != com:
+    #  print 'Goal!'
+#  else:
+    #  print 'Oops...'
+
+
+
+
+
+
 
